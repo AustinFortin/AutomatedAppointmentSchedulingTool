29c51de468c0b388cb8bd4134c00046f6d1e43c9
Austin Miller <Austin>
2024-03-04 13:02:52 -0800

    first upload of project starter

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
     return (
         <div id="footer">
-            Made with 💔 by Fazil.
+            asdikfjaisudfh aisuhfuiasybdf kuasdhbf kuaasydhgf uilksdhfilzsdhfilashdfaosdjfoasjdf
         </div>
     )
 }

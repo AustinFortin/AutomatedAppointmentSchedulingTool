ba5a00db3240474650f96a6e291a75fc0521cd9b
Kartik <102050475+kartikarora003@users.noreply.github.com>
2024-03-07 12:51:31 -0800

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
     return (
         <div id="footer">
-            asdikfjaisudfh aisuhfuiasybdf kuasdhbf kuaasydhgf uilksdhfilzsdhfilashdfaosdjfoasjdf
+            HI THIS APPLICATION IS CREATED BY KARTIK, MEHAK AND AUSTIN
         </div>
     )
 }
